d584c0603534c8d5009473e6025bed8c9280c46d
Artur Francisco Feldhaus <arturfeldhaus12@gmail.com>
2022-02-08 16:14:59 -0300

    Corrigi, uns erros no primeiro codigo.

--- listasexercicio03/Program.cs
+++ listasexercicio03/Program.cs
@@ -6,45 +6,45 @@ namespace listasexercicio03
     {
         static void Main(string[] args)
         {
-            int[] NUMEROS = new int[15];
+            double[] NUMEROS = new double[15];
 
-            Console.WriteLine("Digite o Primeiro numero");
-            NUMEROS[0] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Segundo numero");
-            NUMEROS[1] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Terceiro numero");
-            NUMEROS[2] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Quarto numero");
-            NUMEROS[3] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Quinto numero");
-            NUMEROS[4] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Sexto numero");
-            NUMEROS[5] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Sétimo numero");
-            NUMEROS[6] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Oitavo numero");
-            NUMEROS[7] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Nono numero");
-            NUMEROS[8] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo numero");
-            NUMEROS[9] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo Primeiro numero");
-            NUMEROS[10] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo Segundo numero");
-            NUMEROS[11] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo Terceiro numero");
-            NUMEROS[12] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo Quarto numero");
-            NUMEROS[13] = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o Decimo Quinto numero");
-            NUMEROS[14] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[0] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[1] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[2] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[3] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[4] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[5] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[6] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[7] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[8] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[9] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[10] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[11] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[12] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[13] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma altura!!!");
+            NUMEROS[14] = double.Parse(Console.ReadLine());
 
             Array.Sort(NUMEROS);
-            int maior = NUMEROS.Max();
-            int menor = NUMEROS.Min();
-            Console.WriteLine("O maior numero é {0}", maior);
+            double maior = NUMEROS.Max();
+            double menor = NUMEROS.Min();
+            Console.WriteLine("A maior altura é {0}", maior);
             Console.Read();
-            Console.WriteLine("O menor numero é {0}", menor);
+            Console.WriteLine("A menor altura é {0}", menor);
            
 
             Console.ReadLine();
